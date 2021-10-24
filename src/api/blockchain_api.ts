07d6a07d8284a3221094c5749949325606218217
Phan Huynh Duc <ducph@nal.vn>
2021-10-25 00:23:27 +0700

    Add env of api

--- src/api/blockchain_api.ts
+++ src/api/blockchain_api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: process.env.NEXT_PUBLIC_BLOCKCHAIN_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
